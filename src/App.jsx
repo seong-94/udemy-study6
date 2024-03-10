73f459cd996bdbe6b72fe0413fb547036bc9e2ff
seong-94 <togussla@naver.com>
2024-03-10 22:05:32 +0900

    강의 188

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -46,7 +46,7 @@ function App() {
     });
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) => {
       prevPickedPlaces.filter((place) => {
         place.id !== selectedPlace.current;
@@ -59,7 +59,7 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
